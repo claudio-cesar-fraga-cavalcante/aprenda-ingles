6bf26bd0d334b2b8d6470b8de5ccb25fb19e4e48
claudio-cesar-fraga-cavalcante <claudio.cavalcante@gmail.com>
2019-12-07 18:39:34 +0000

    Alterações nos arquivos

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -9,8 +9,24 @@ import { FRASES } from './frases-mock'
 })
 export class PainelComponent implements OnInit {
   public frases: Frase[] = FRASES
+  public resposta: String
+  public rodada: number = 0
+  public rodadaFrase: Frase
+
   constructor() {
-    console.log(this.frases)
+    this.rodadaFrase = this.frases[this.rodada]
+  }
+
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value
+  }
+
+  public verificarResposta(): void {
+    this.rodada++
+    this.rodadaFrase = this.frases[this.rodada]
+    if (this.resposta == this.rodadaFrase.frasePtBr) {
+      console.log("Acertou")
+    }
   }
 
   ngOnInit() {
