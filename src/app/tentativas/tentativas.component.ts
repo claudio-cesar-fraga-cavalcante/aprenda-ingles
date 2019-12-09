d6ff8e1ad08e0574eced530b5511cf5f4e78a713
claudio-cesar-fraga-cavalcante <claudio.cavalcante@gmail.com>
2019-12-09 20:41:12 +0000

    Alterações no componente tentativas

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Coracao } from '../shared/coracao.model'
 
 @Component({
   selector: 'app-tentativas',
@@ -6,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tentativas.component.css']
 })
 export class TentativasComponent implements OnInit {
-  public coracao_vazio:String = "/assets/coracao_vazio.png"
-  public coracao_cheio:String = "/assets/coracao_cheio.png"
-  constructor() { }
+
+  public coracoes: Coracao[] = [
+    new Coracao(true),
+    new Coracao(true),
+    new Coracao(true)
+  ]
+
+  constructor() {
+   }
 
   ngOnInit() {
   }
