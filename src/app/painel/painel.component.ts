06dc2af8fbba06fb982b93d5afc70fd54dbb4f16
claudio-cesar-fraga-cavalcante <claudio.cavalcante@gmail.com>
2019-12-09 01:20:36 +0000

    Evolução do projeto

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -9,12 +9,14 @@ import { FRASES } from './frases-mock'
 })
 export class PainelComponent implements OnInit {
   public frases: Frase[] = FRASES
-  public resposta: String
+  public resposta: String = ""
   public rodada: number = 0
   public rodadaFrase: Frase
+  public progresso: number = 0
+  private porcento: number = 100
 
   constructor() {
-    this.rodadaFrase = this.frases[this.rodada]
+    this.atualizaRodada()
   }
 
   public atualizaResposta(resposta: Event): void {
@@ -22,11 +24,28 @@ export class PainelComponent implements OnInit {
   }
 
   public verificarResposta(): void {
-    this.rodada++
-    this.rodadaFrase = this.frases[this.rodada]
+
     if (this.resposta == this.rodadaFrase.frasePtBr) {
-      console.log("Acertou")
+      alert('A traudução está correta')
+      //trocar pergunta da rodada
+      this.rodada++
+
+      // atualiza o objeto rodada frase
+      this.atualizaRodada()
+
+      // atualiza o progesso
+      this.progresso = this.progresso + (this.porcento / this.frases.length)
     }
+    else {
+      alert('A traudução está errada')
+    }
+
+    // limpar resposta
+    this.resposta = ""
+  }
+
+  public atualizaRodada(): void {
+    this.rodadaFrase = this.frases[this.rodada]
   }
 
   ngOnInit() {
